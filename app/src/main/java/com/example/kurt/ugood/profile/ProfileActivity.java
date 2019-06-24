bbbbba05d9373f03d24de1f2e38a32650419127d
KTaratun <KurtTaratun@gmail.com>
2019-06-24 09:55:24 -0400

    Finished Settings Activity.
    - Change Username
    
    - Change Profile Pic
    
    - Logout

--- app/src/main/java/com/example/kurt/ugood/profile/ProfileActivity.java
+++ app/src/main/java/com/example/kurt/ugood/profile/ProfileActivity.java
@@ -1,19 +1,28 @@
 package com.example.kurt.ugood.profile;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.kurt.ugood.R;
 import com.example.kurt.ugood.firebase.FirebaseFunctions;
+import com.example.kurt.ugood.firebase.GlideApp;
 import com.example.kurt.ugood.main.MainActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.net.URI;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -49,6 +58,29 @@ public class ProfileActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(ProfileActivity.this, SettingsActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        LoadImageFromFirebase();
+    }
+
+    private void LoadImageFromFirebase()
+    {
+        // Reference to an image file in Cloud Storage
+        String curUserID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("Users").child(curUserID).child("Profile Pic");
+
+        Task<Uri> newTask = storageReference.getDownloadUrl();
+        newTask.addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                ImageView image = (ImageView)findViewById(R.id.profilePic);
+
+                // Download directly from StorageReference using Glide
+                // (See MyAppGlideModule for Loader registration)
+                GlideApp.with(getApplicationContext() /* context */)
+                        .load(uri)
+                        .into(image);
             }
         });
     }
--- app/src/main/java/com/example/kurt/ugood/profile/SettingsActivity.java
+++ app/src/main/java/com/example/kurt/ugood/profile/SettingsActivity.java
@@ -1,27 +1,47 @@
 package com.example.kurt.ugood.profile;
 
 import android.app.DialogFragment;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.kurt.ugood.R;
+import com.example.kurt.ugood.login.LoginActivity;
+import com.example.kurt.ugood.main.MainActivity;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    private TextView changeUser, changePhoto;
+    private Button back;
+    private TextView changeUser, changePhoto, logout;
     public static final int PICK_IMAGE = 1;
+    private StorageReference storageReference;
+    private Uri filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_settings_activity);
 
+        storageReference = FirebaseStorage.getInstance().getReference().child("Users");
+
         changeUser = (TextView) findViewById(R.id.change_username);
         changePhoto = (TextView) findViewById(R.id.change_photo);
+        logout = (TextView) findViewById(R.id.logout);
+        back = (Button) findViewById(R.id.backButton);
 
         changeUser.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +58,22 @@ public class SettingsActivity extends AppCompatActivity {
                 changeName.show(getFragmentManager(), "ChangeUserPictureFragment");
             }
         });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth fbAuth = FirebaseAuth.getInstance();
+                fbAuth.signOut();
+                startActivity(new Intent(SettingsActivity.this, LoginActivity.class)); //Go back to home page
+            }
+        });
     }
 
     @Override
@@ -46,8 +82,41 @@ public class SettingsActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == PICK_IMAGE) {
-            //TODO: action
-            Log.i("dfdf", "onActivityResult: ");
+            filePath = data.getData();
+
+            if(filePath != null)
+            {
+                final ProgressDialog progressDialog = new ProgressDialog(this);
+                progressDialog.setTitle("Uploading...");
+                progressDialog.show();
+
+                String curUserID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+                StorageReference FBStorageLocation = storageReference.child(curUserID).child("Profile Pic");
+                FBStorageLocation.putFile(filePath)
+                        .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                progressDialog.dismiss();
+                                Toast.makeText(SettingsActivity.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                progressDialog.dismiss();
+                                Toast.makeText(SettingsActivity.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                                double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                        .getTotalByteCount());
+                                progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                            }
+                        });
+            }
         }
     }
 }
